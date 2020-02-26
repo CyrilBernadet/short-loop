e3cf2b398d298e9ba1ecc56253e317f71a809ae5
Cyril Bernadet <cyril.bernadet.e@thalesdigital.io>
2020-02-26 15:17:30 +0100

    Add material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
@@ -11,10 +14,14 @@ import { PlaceFinderComponent } from './place-finder/place-finder.component';
   declarations: [AppComponent, MapComponent, PlaceFinderComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     GoogleMapsModule,
-    AppRoutingModule
+    MatFormFieldModule,
+    MatInputModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
