0bedcb82f23b326068324c620440692024ec8c2e
Cyril Bernadet <cyril.bernadet.e@thalesdigital.io>
2020-02-26 14:42:12 +0100

    Add place finder component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { PlaceFinderComponent } from './place-finder/place-finder.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
-  imports: [BrowserModule, GoogleMapsModule, AppRoutingModule],
+  declarations: [AppComponent, MapComponent, PlaceFinderComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    GoogleMapsModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
