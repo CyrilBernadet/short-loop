135d8789c2b64104a110d8e547d1dd3b7d883128
Cyril Bernadet <cyril.bernadet.e@thalesdigital.io>
2020-02-27 14:17:59 +0100

    Add delete button on places list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { PlacesListComponent } from './places-list/places-list.component';
 
 @NgModule({
   declarations: [AppComponent, MapComponent, PlacesListComponent],
-  imports: [BrowserModule, GoogleMapsModule, AppRoutingModule],
+  imports: [BrowserModule, MatIconModule, GoogleMapsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
